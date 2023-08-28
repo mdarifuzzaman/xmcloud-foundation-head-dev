ff797388dde640eda5d1955a5f6d6c523da84724
Arif Uzzaman <mdar@sitecore.net>
2023-08-28 18:57:18 +1000

    fix bug

--- src/sxastarter/src/Document.tsx
+++ src/sxastarter/src/Document.tsx
@@ -1,9 +1,9 @@
 export default function StyleDocument() {
   return (
     <>
-      <script src="js/jquery.js"></script>
-      <script src="js/modernizr.js" />
-      <script src="js/slick.js"></script>
+      <script async src="js/jquery.js"></script>
+      <script async src="js/modernizr.js" />
+      <script async src="js/slick.js"></script>
       <link rel="stylesheet" href="css/normalize.css" />
       <link rel="stylesheet" href="css/foundation.css" />
       <link rel="stylesheet" href="css/slick.css" />

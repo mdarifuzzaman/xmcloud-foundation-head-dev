31c5e70ee431ca0fa20b3a773a3b294b537c5b76
Arif Uzzaman <arif.uzzaman@sitecore.com>
2023-09-12 11:46:32 +1000

    refactor

--- src/sxastarter/src/pages/api/engage.ts
+++ src/sxastarter/src/pages/api/engage.ts
@@ -2,16 +2,15 @@ import { init } from '@sitecore/engage';
 
 let engage: any;
 
-
-const loadEngage = async() => {
-    engage = await init({
-        clientKey: "4b68aa42a5559c220ca6f1c107303615",
-        targetURL: "https://api-engage-ap.sitecorecloud.io​",
-        cookieDomain: ".sxastarter.localhost",
-        cookieExpiryDays: 365,
-        forceServerCookieMode: false,
-        includeUTMParameters: true
-    });
+const loadEngage = async () => {
+  engage = await init({
+    clientKey: '4b68aa42a5559c220ca6f1c107303615',
+    targetURL: 'https://api-engage-ap.sitecorecloud.io​',
+    cookieDomain: '.sxastarter.localhost',
+    cookieExpiryDays: 365,
+    forceServerCookieMode: false,
+    includeUTMParameters: true,
+  });
 };
 
 loadEngage();

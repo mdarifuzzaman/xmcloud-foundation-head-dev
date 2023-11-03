636a06c44eb0a92be0f57c3e792403a1aaf285af
Arif Uzzaman <arif.uzzaman@sitecore.com>
2023-11-03 14:39:40 +1100

    storybook added

--- src/sxastarter/tailwind.config.js
+++ src/sxastarter/tailwind.config.js
@@ -3,6 +3,8 @@ module.exports = {
     './src/pages/**/*.{js,ts,jsx,tsx}',
     './src/components/**/*.{js,ts,jsx,tsx}',
     './src/*.{js,ts,jsx,tsx}',
+    // Or if using `src` directory:
+    './src/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   safelist: [
     {

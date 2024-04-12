081b154f1ae6a6f8239e437d7dd767d7a66e8ada
Arif Uzzaman <arif.uzzaman@sitecore.com>
2024-04-12 12:24:07 +1000

    some changes

--- src/sxastarter/src/pages/[[...path]].tsx
+++ src/sxastarter/src/pages/[[...path]].tsx
@@ -10,45 +10,41 @@ import {
   StaticPath,
 } from '@sitecore-jss/sitecore-jss-nextjs';
 import { handleEditorFastRefresh } from '@sitecore-jss/sitecore-jss-nextjs/utils';
-import { SitecorePageProps } from 'lib/page-props';
+//import { SitecorePageProps } from 'lib/page-props';
 import { sitecorePagePropsFactory } from 'lib/page-props-factory';
 import { componentBuilder } from 'temp/componentBuilder';
 import { sitemapFetcher } from 'lib/sitemap-fetcher';
 
-const SitecorePage = ({
-  notFound,
-  componentProps,
-  layoutData,
-  headLinks,
-}: SitecorePageProps): JSX.Element => {
+const SitecorePage = (props: any): JSX.Element => {
+  console.log("Sitecore page props", props);
   useEffect(() => {
     // Since Sitecore editors do not support Fast Refresh, need to refresh editor chromes after Fast Refresh finished
     handleEditorFastRefresh();
   }, []);
 
-  if (notFound || !layoutData.sitecore.route) {
+  if (props.notFound || !props.layoutData.sitecore.route) {
     // Shouldn't hit this (as long as 'notFound' is being returned below), but just to be safe
     return <NotFound />;
   }
 
-  const isEditing = layoutData.sitecore.context.pageEditing;
+  const isEditing = props.layoutData.sitecore.context.pageEditing;
   const isComponentRendering =
-    layoutData.sitecore.context.renderingType === RenderingType.Component;
+  props.layoutData.sitecore.context.renderingType === RenderingType.Component;
 
   return (
-    <ComponentPropsContext value={componentProps}>
+    <ComponentPropsContext value={props.componentProps}>
       <SitecoreContext
         componentFactory={componentBuilder.getComponentFactory({ isEditing })}
-        layoutData={layoutData}
+        layoutData={props.layoutData}
       >
         {/*
           Sitecore Pages supports component rendering to avoid refreshing the entire page during component editing.
           If you are using Experience Editor only, this logic can be removed, Layout can be left.
         */}
         {isComponentRendering ? (
-          <EditingComponentPlaceholder rendering={layoutData.sitecore.route} />
+          <EditingComponentPlaceholder rendering={props.layoutData.sitecore.route} />
         ) : (
-          <Layout layoutData={layoutData} headLinks={headLinks} />
+          <Layout layoutData={props.layoutData} headLinks={props.headLinks} />
         )}
       </SitecoreContext>
     </ComponentPropsContext>

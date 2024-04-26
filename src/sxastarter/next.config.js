cdfaaa39165376f41ba85058dfba32b88b64f92d
Arif Uzzaman <arif.uzzaman@sitecore.com>
2024-04-26 14:52:25 +1000

    update media path

--- src/sxastarter/next.config.js
+++ src/sxastarter/next.config.js
@@ -59,8 +59,8 @@ const nextConfig = {
       },
       // media items
       {
-        source: `/-/:path*`,
-        destination: `${process.env.SITECORE_EDGE_HOST}/:path*`,
+        source: `/au/-/:path*`,
+        destination: `${process.env.SITECORE_EDGE_HOST}/professionaf048-xmcloudmdarpro-devcollection-a70b/:path*`,
       },
       // healthz check
       {

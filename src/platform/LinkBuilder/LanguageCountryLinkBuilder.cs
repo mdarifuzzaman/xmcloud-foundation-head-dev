3b1c6ab447e4d97983a5e6ea11d5f862009a4b4e
Arif Uzzaman <arif.uzzaman@sitecore.com>
2024-04-30 10:49:01 +1000

    added required language item builder

---
+++ src/platform/LinkBuilder/LanguageCountryLinkBuilder.cs
@@ -0,0 +1,41 @@
+﻿using Sitecore.Data.Items;
+using Sitecore.Links.UrlBuilders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace XmCloudSXAStarter.LinkBuilder
+{
+    public class LanguageCountryLinkBuilder : ItemUrlBuilder
+    {
+        public LanguageCountryLinkBuilder(DefaultItemUrlBuilderOptions defaultOptions) : base(defaultOptions)
+        {
+
+        }
+        public override string Build(Item item, ItemUrlBuilderOptions options)
+        {
+            var itemUrl = base.Build(item, options);
+            var supportedLanguages = new List<string> { "en", "ja-JP" };
+            var supportedCountries = new List<string> { "au", "jp" };
+
+            foreach (var lan in supportedLanguages)
+            {
+                foreach (var country in supportedCountries)
+                {
+                    var currentFormat = $"{lan}/{country}";
+                    var newFormat = $"{country}/{lan}";
+                    if (itemUrl.Contains(currentFormat))
+                    {
+                        itemUrl = itemUrl.Replace(currentFormat, newFormat);
+                        break;
+                    }
+                }
+            }
+
+            return itemUrl;
+        }
+
+
+    }
+}

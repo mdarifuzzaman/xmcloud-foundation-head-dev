bea587439de78c26b5eba792998d6e9f91d22e62
Arif Uzzaman <arif.uzzaman@sitecore.com>
2024-05-02 13:57:25 +1000

    added meta

---
+++ src/sxastarter/src/components/Meta.tsx
@@ -0,0 +1,20 @@
+
+import { ComponentConsumerProps, TextField, withSitecoreContext } from '@sitecore-jss/sitecore-jss-nextjs'
+import Head from 'next/head'
+import React from 'react'
+
+type MetaType = ComponentConsumerProps &{
+  fields: {
+    authorMeta: TextField
+  }
+}
+const Meta = (props: MetaType) => {
+  console.log("Metadata", props);
+  return (
+    <Head>
+      <meta name='author' content={props.fields.authorMeta?.value + ""}></meta>
+    </Head>
+  )
+}
+
+export default withSitecoreContext()<MetaType>(Meta);
